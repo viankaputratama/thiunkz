dbacb6783895bd7078fd8ebdd56a27482aa8cb5c
viankaputratama <46253844+viankaputratama@users.noreply.github.com>
2018-12-30 11:11:06 +0700

    Add files via upload

---
+++ ops.php
@@ -0,0 +1,15 @@
+<?php
+/*
+| -------------------------------------------------------------------------------
+| Author            : Mathin Mochammad
+| Template Name     : Muvimag V2
+| -------------------------------------------------------------------------------
+*/
+include('header.php'); ?>
+
+      <div class="well text-center">
+        <div style="text-align: center;"><img alt="ketauan ctrl+u" src="https://i.imgur.com/t7Z0nVI.gif" title="Ops ctrl+u">
+<p><font size="46"  style="color:#A30202;">LOE MASUK SINI MAU NGECLONE YA??</font></p>
+      </div>
+
+<?php include('footer.php'); ?>
